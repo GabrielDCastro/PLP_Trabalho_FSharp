67bc7053b14c4e55db42382665c0402089ea4d69
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-04-01 08:54:57 -0300

    code4

---
+++ aula 31-03/code4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AulaPLP{
+    public static void main(String[] args){
+        for(int i=0; i<8; i++){
+            System.out.println(i);
+            if(i==5){
+                break;
+            }
+        }
+    }   
+}
