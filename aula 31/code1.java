4e96cbbe81e5f7afdc68fbdaf60cbf8363e4161b
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-03-31 23:09:12 -0300

    Create code

---
+++ aula 31/code1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AulaPLP {
+    public static void main(String[] args){
+        int num;
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        num = leitura.nextInt();
+        
+        if (num>0){
+            System.out.println("O número é positivo.");       
+        }else{
+            System.out.println("O número é negativo.");
+        }
+    }   
+}
