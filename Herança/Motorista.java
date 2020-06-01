002a1d2c934a2771069314c3bef3341cee6d3c50
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-06-01 11:40:30 -0300

    Classe motorista

---
+++ Herança/Motorista.java
@@ -0,0 +1,19 @@
+package herancas;
+
+public class Motorista extends Pessoa{
+    private String cnh;
+    
+    public Motorista(String nome, String cpf, String _cnh) {
+        super(nome, cpf);
+        this.cnh = _cnh;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+        
+}
