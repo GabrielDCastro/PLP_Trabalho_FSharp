c450d0b4f81c136df164b76eef66c9e37f99dc8e
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-06-02 19:17:48 -0300

    main

---
+++ Polimorfismo slide 24/Plp.java
@@ -0,0 +1,18 @@
+package plp;
+/**
+ *
+ * @author gabri
+ */
+public class Plp {
+    public static void main(String[] args) {
+        Vaca fofinha = new Vaca("fofinha");
+        fofinha.emitirSom();
+        
+        Gato chaninho = new Gato ("Chaninho");
+        chaninho.emitirSom();
+        
+        Carneiro satan = new Carneiro ("satan");
+        satan.emitirSom();
+    }
+    
+}
