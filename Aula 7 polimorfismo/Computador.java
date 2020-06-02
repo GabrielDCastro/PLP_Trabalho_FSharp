969e302e79e3468b20f7431f086fd63c173a301f
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-06-02 19:41:13 -0300

    Classe abstrata computador

---
+++ Aula 7 polimorfismo/Computador.java
@@ -0,0 +1,15 @@
+package teste;
+
+public abstract class Computador {
+
+    public abstract String getRAM();
+
+    public abstract String getHDD();
+
+    public abstract String getCPU();
+    
+    @Override
+    public String toString(){
+        return "RAM = "+this.getRAM() + " HDD = "+this.getHDD()+" CPU = "+this.getCPU();
+    }
+}
