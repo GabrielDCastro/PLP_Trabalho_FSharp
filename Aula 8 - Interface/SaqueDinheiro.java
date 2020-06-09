cbaa1961bbdcc70edfa785326fd711e8244f9e7a
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-06-09 12:24:11 -0300

    classe

---
+++ Aula 8 - Interface/SaqueDinheiro.java
@@ -0,0 +1,15 @@
+package Interface;
+
+public class SaqueDinheiro {
+    private int valor;
+
+    public SaqueDinheiro(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+    
+    
+}
